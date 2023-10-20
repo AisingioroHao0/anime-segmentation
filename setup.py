4da2eb19e1879777ade2a241557eb65041f3bdc3
AisingioroHao0 <AisingioroHao0@outllok.com>
2023-10-20 19:21:54 +0800

    restruct ,support for git install

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="anime_segmentation",
+    version="0.1.0",
+    description="Segmentation for anime character",
+    keywords="segmentation anime character",
+    license="Apache",
+    author="aihao",
+    author_email="aihao2000@outlook.com",
+    url="https://github.com/aihao2000/anime_segmentation.git",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    python_requires=">=3.8.0",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "torch",
+        "torchvision",
+        "pytorch-lightning",
+        "pillow",
+        "kornia",
+        "timm",
+        "opencv-python",
+        "gradio",
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
